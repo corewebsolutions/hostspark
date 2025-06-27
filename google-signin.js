a433b9ba42a996d001b767d97ba8acb9eafd1b36
corewebsolutions <contact@corewebsolutions.com>
2025-06-27 11:37:57 -0400

    Create google-signin.js

---
+++ google-signin.js
@@ -0,0 +1,29 @@
+
+window.onload = function () {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (!code) {
+      alert("Missing Google code. Try signing in again.");
+      return;
+    }
+    $.ajax({
+    url: "https://x8ki-letl-twmt.n7.xano.io/api:xAumndFJ/google-login",
+    type: "POST",
+    data: {
+      code: code
+    },
+    success: function (response) {
+        if (response.authToken) {
+          localStorage.setItem("authToken", response.authToken);
+          window.location.href = "/welcome"; // Redirect after login
+        } else {
+          alert("Login failed. Please try again.");
+        }
+    },
+      error: function (xhr, status, error) {
+        console.error("OAuth Error:", error);
+        alert("Something went wrong during login.");
+      }
+  	});
+};
