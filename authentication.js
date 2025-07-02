74e85a941c41ba240669cf2f57c8acd7a292751c
corewebsolutions <contact@corewebsolutions.com>
2025-07-01 22:51:54 -0400

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const $onboardingGoogle = $('#signup-google-data-form'); // onboarding form - google users
     const $onboardingManual = $('#signup-manual-data-form'); // onboarding form - manual users
     const $forgotPassForm = $('#forgot-password-form'); // forgot password form
+    const $loginForm = $('#login-form'); // login form
 
     // check for redirected users after sign up for additional questions...
     if (localStorage.getItem("authToken")) { 
@@ -31,6 +32,52 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const email = $('#signup-email').val().trim();
         const password = $('#signup-password').val().trim();
+
+        if (!email || !password) {
+        showFormError('Email and password are required.');
+        setLoadingState(false);
+        return;
+        }
+
+        $.ajax({
+        url: baseURL + 'api:xAumndFJ/auth/signup',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ email, password }),
+        success: function (response) {
+
+            localStorage.setItem("authToken", response.authToken);
+            localStorage.setItem("authMode", "manual");
+
+            if (response.new_user == false) {
+                window.location.href = "/welcome"; 
+            } else {
+                window.location.href = "/auth/sign-up"; 
+            }
+
+            setLoadingState(false);
+
+        },
+        error: function (xhr) {
+            const err = xhr.responseJSON?.message || 'Signup failed. Please try again.';
+            showFormError(err);
+            setLoadingState(false);
+        }
+        });
+
+    });
+
+    //Login form submitted...
+    $loginForm.on('submit', function (e) {
+
+        e.preventDefault();
+
+
+        // Flag this form as active
+        setLoadingState(true, this);
+
+        const email = $('#login-email').val().trim();
+        const password = $('#login-password').val().trim();
 
         if (!email || !password) {
         showFormError('Email and password are required.');
