a0bdb80d2d874a7a0b01605e8cb546a1b7529d40
corewebsolutions <contact@corewebsolutions.com>
2025-07-11 12:16:43 -0400

    Create create-event.js

---
+++ create-event.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  updateStepNumberActiveState(); // initiate step navigation
+
+  $('.app-vertical-tabs__button').on('click', function () { // click handler for step nav
+    setTimeout(() => {
+      updateStepNumberActiveState();
+    }, 50);
+  });
+
+
+});
+
+
+
+// Nav Steps Functionality
+function updateStepNumberActiveState() {
+    $('.step-number').removeClass('active');
+    $('.app-vertical-tabs__button.w--current').each(function () {
+      $(this).find('.step-number').addClass('active');
+    });
+}
